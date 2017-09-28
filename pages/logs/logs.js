812cb7e1b5addc1650f892c21d6ee624fd308604
xueweiguo <xueweiguo@hotmail.com>
2017-09-28 23:51:19 +0800

    log输出功能实现

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,9 +1,17 @@
 //logs.js
+const app = getApp()
 const util = require('../../utils/util.js')
 
 Page({
   data: {
     logs: []
   },
- 
+
+  onLoad:function(){
+    this.setData({logs:app.globalData.logs.map(function(x){
+        var timestr = x.time.getHours() + ':' + x.time.getMinutes() + ':' + x.time.getSeconds();
+        return timestr + ' ' + x.message
+      })
+    })
+  }
 })
